a2ad221be6e5d2a4f0dc2fd549a42fcef2943b85
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-15 22:41:44 +0300

    Task7 complete

---
+++ Homework/src/Task9.java
@@ -0,0 +1,9 @@
+public class Task9 {
+    public static void main(String[] args){
+     int[] nums  = {18, 60, 90, 110};
+        for(int i = 0; i<nums.length; i++)
+        {
+            System.out.println(nums[i] + "*2=" + nums[i]*2);
+        }
+    }
+}
