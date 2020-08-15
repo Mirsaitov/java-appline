735af677a6275fa1f4c599b69ad43271fbf1c13c
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-15 17:12:30 +0300

    Task5 complete

---
+++ Homework/src/Task5.java
@@ -0,0 +1,28 @@
+public class Task5 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 1;
+
+        System.out.println(b + "x" + a + "=" + a*b);
+        b++;
+        System.out.println(b +"x" + a + "=" + a*b);
+        b++;
+        System.out.println(b +"x" + a + "=" + a*b);
+        b++;
+        System.out.println(b +"x" + a + "=" + a*b);
+        b++;
+        System.out.println(b +"x" + a + "=" + a*b);
+        b++;
+        System.out.println(b +"x" + a + "=" + a*b);
+        b++;
+        System.out.println(b +"x" + a + "=" + a*b);
+        b++;
+        System.out.println(b +"x" + a + "=" + a*b);
+        b++;
+        System.out.println(b +"x" + a + "=" + a*b);
+        b++;
+        System.out.println(b +"x" + a + "=" + a*b);
+
+    }
+}
+
