2f36915a4db22c9bff3ce76ad7124e7ec94cbd84
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-15 19:00:17 +0300

    Task7 complete

---
+++ Homework/src/Task7.java
@@ -0,0 +1,22 @@
+public class Task7 {
+    public static void main(String[] args) {
+        int x = 13;
+        int y = 22;
+        int z = 21;
+        int number = 132;
+        if (number == x) {
+            System.out.println("Данное значение имеется в константах");
+        }
+        else if(number == y) {
+            System.out.println("Данное значение имеется в константах");
+        }
+        else if(number == z) {
+            System.out.println("Данное значение имеется в константах");
+        }
+        else {
+            System.out.println("Такой константы нет!");
+
+        }
+    }
+}
+
