302f00986b820d98151f4cb8bbbe4e1ba447101a
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-15 18:16:12 +0300

    Task6 complete

---
+++ Homework/src/Task6.java
@@ -0,0 +1,15 @@
+public class Task6 {
+    public static void main(String[] args) {
+        int x = 13;
+        int y = 14;
+        int z = 12;
+        int srsum = (x+y+z)/3;
+        System.out.println(srsum);
+
+        int delenie = srsum/2;
+        if(delenie > 3)
+            System.out.println("Программа выполнена корректно");
+
+
+    }
+}
