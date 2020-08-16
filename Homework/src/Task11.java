603b33cad1a493bac6d29430b2845356eea2d5c9
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-17 00:44:17 +0300

    Task11 переделан

--- Homework/src/Task11.java
+++ Homework/src/Task11.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class Task11 {
     public static void main(String[] args){
-    String hi = "Hello world!";
-        System.out.println(hi);
+    Scanner input = new Scanner(System.in);
+        System.out.println("Введите строковый данные: ");
+    String hi = input.nextLine();
+        System.out.println("Результат: " + hi);
     }
 }
