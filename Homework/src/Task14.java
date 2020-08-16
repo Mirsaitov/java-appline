0ccbf36fbdc1972971cf9795e6909c3bd5346e47
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-16 15:15:38 +0300

    Task14 complete

--- Homework/src/Task14.java
+++ Homework/src/Task14.java
@@ -1,10 +1,10 @@
 public class Task14 {
     public static void main(String[] args){
-       String S = "124";
-        System.out.println(S);
+        String S = "124";
+            System.out.println(S);
         int X = Integer.parseInt (S.trim ());
-        System.out.println(X);
+            System.out.println(X);
         double Y = (double) X;
-        System.out.println(Y);
+            System.out.println(Y);
     }
     }
