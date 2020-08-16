660199ed398e715a98e6539ace806302dcd270b2
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-16 16:08:56 +0300

    Task15 complete

--- Homework/src/Task15.java
+++ Homework/src/Task15.java
@@ -1,6 +1,6 @@
 public class Task15 {
     public static void main(String[] args) {
-        int[] nums = {91, 22, 41, 52, 34, 15, 0, 72, 35, 8};
+        int[] nums = {91, 22, 41, 52, 34, 15, 2, 72, 35, 8};
         for (int i = nums.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (nums[j] > nums[j + 1]) {
