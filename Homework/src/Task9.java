b6c605da1601b310d92a4459d5926111cb644b6c
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-16 21:40:17 +0300

    Task9 переделан

--- Homework/src/Task9.java
+++ Homework/src/Task9.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 public class Task9 {
     public static void main(String[] args){
-     int[] nums  = {18, 60, 90, 110};
+        Scanner input = new Scanner(System.in);
+
+            System.out.print("Шаг 1: " + "\n" + "Введите размер массива: ");
+
+        int size = input.nextInt();
+        int nums[] = new int[size];
+
+            System.out.println("Шаг 2: " + "\n" +"Введите данные массива №1: ");
+
+        for (int i = 0; i < size; i++) {
+                    nums[i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+                    System.out.println("Введите данные массива" + " №" + (i+2) + ": ");
+        }
+
+            System.out.println("Шаг 3: " + "\n" + "Умножаем каждый елемент массива на 2: ");
         for(int i = 0; i<nums.length; i++)
-        {
-            System.out.println(nums[i] + "*2=" + nums[i]*2);
+                {
+           System.out.println(nums[i] + "*2=" + nums[i]*2);
         }
     }
 }
