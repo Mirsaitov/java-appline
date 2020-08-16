ca458d1fa7d0f9ce1de8509cf8a3fb57924fc01a
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-16 21:10:53 +0300

    Task6 переделан

--- Homework/src/Task6.java
+++ Homework/src/Task6.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 public class Task6 {
     public static void main(String[] args) {
-        int x = 13;
-        int y = 14;
-        int z = 12;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число x: ");
+        int x = input.nextInt();
+        System.out.print("Введите число y: ");
+        int y = input.nextInt();
+        System.out.print("Введите число z: ");
+        int z = input.nextInt();
         int srsum = (x+y+z)/3;
-        System.out.println(srsum);
+        System.out.println("Среднее арифметическое число: " + srsum);
 
         int delenie = srsum/2;
         if(delenie > 3)
