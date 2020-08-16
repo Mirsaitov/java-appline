2d5c16ccf21496e00af43bf1032f7a3eaa53505f
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-16 21:14:49 +0300

    Task7 переделан

--- Homework/src/Task7.java
+++ Homework/src/Task7.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 public class Task7 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число: ");
         int x = 13;
         int y = 22;
         int z = 21;
-        int number = 132;
+        int number = input.nextInt();;
         if (number == x) {
             System.out.println("Данное значение имеется в константах");
         }
