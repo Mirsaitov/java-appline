2c0a57e598982ce4dff9be0e2f877b2379a6a29d
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-16 23:22:11 +0300

    Task9 переделан

--- Homework/src/Task10.java
+++ Homework/src/Task10.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
 public class Task10 {
     public static void main(String[] args) {
-        int[][] nums  = {{1, 2, 3,4}, {5, 6, 7,8}, {9,10, 11, 12}
-        };
-        for(int i = 0; i<= 0; i++)
-        {
-            for(int t = 0; t<= 3; t++) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Шаг 1: " + "\n" + "Введите количество строк массива: ");
+        int size = input.nextInt();
+        System.out.print("Введите количество столбцов массива: ");
+        int size2 = input.nextInt();
+        int nums[][] = new int[size][size2];
 
-                System.out.println(nums[i][t] + "*3=" + nums[i][t] * 3);
+
+
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                System.out.print("Введите элемент массива[" + i + "][" + j + "]:");
+                nums[i][j] = input.nextInt();
+
+
+                }
+
             }
+
+
         }
     }
-}
+
