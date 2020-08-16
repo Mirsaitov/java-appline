7f1cb815905aeaa806c22e7e8c261cc5ca0dcb0f
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-16 14:01:06 +0300

    Task12 complete

---
+++ Homework/src/Task12.java
@@ -0,0 +1,9 @@
+public class Task12 {
+    public static void main(String[] args) {
+        String hi = "Hello my world !";
+        String hi2 = hi.replaceAll(" ", "");
+
+        System.out.println(hi2);
+
+    }
+}
