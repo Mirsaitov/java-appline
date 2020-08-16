8ba97e17737a09966c6c09cef09bd91eee13e1ac
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-17 00:21:43 +0300

    Task8 переделан

--- Homework/src/Task8.java
+++ Homework/src/Task8.java
@@ -1,10 +1,7 @@
 public class Task8 {
     public static void main(String[] args){
-        int i = 1;
-        while (i <= 50) {
-            System.out.print(i);
-            i++;
-            System.out.print("\n");
-        }
+       for (int i = 1; i <= 50; i++) {
+           System.out.println(i);
+       }
     }
 }
