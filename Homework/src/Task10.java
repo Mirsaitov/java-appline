501d7e42bdf84760409918adbb75535d57395157
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-17 00:11:23 +0300

    Task10 переделан

--- Homework/src/Task10.java
+++ Homework/src/Task10.java
@@ -8,20 +8,22 @@ public class Task10 {
         int size2 = input.nextInt();
         int nums[][] = new int[size][size2];
 
-
-
-
+        System.out.print("Шаг 2: " + "\n");
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums[i].length; j++) {
                 System.out.print("Введите элемент массива[" + i + "][" + j + "]:");
                 nums[i][j] = input.nextInt();
-
-
                 }
-
             }
+        System.out.println("Шаг 3: " + "\n" + "Умножаем каждый елемент массива первой строки на 3: ");
+        for(int i = 0; i<= 0; i++)
+        {
+            for(int t = 0; t< size2; t++) {
 
-
+                System.out.println(nums[i][t] + "*3=" + nums[i][t] * 3);
+            }
+          }
+         }
         }
-    }
+
 
