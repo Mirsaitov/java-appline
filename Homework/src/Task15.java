3ca9c659c06c56af8459f72afae0ef24e2f98db4
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-17 23:33:03 +0300

    Task15 и 16 complete

--- Homework/src/Task15.java
+++ Homework/src/Task15.java
@@ -5,11 +5,11 @@ public class Task15 {
         System.out.print("Введите размер массива: ");
         int size = input.nextInt();
         int nums[] = new int[size];
-        for (int i = 0; i < size; i++) {
+        for (int a = 0; a < size; a++) {
             System.out.println("Введите данные массива" + " №" + (i+1) + ": ");
-            nums[i] = input.nextInt();
+            nums[a] = input.nextInt();
         }
-          for (int i = nums.length - 1; i > 0; i--) {
+          for (int i = size - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (nums[j] > nums[j + 1]) {
                     int tmp = nums[j];
@@ -18,7 +18,7 @@ public class Task15 {
                 }
             }
         }
-        for (int q = 0; q < nums.length; q++)
+        for (int q = 0; q < size; q++)
         System.out.println(nums[q]);
     }
 }
