2be5b538e5b4defbd77362fb6786e088bf1f9e7e
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-08-24 22:35:29 +0300

    Task15 и 16 complete

--- Homework/src/Task16.java
+++ Homework/src/Task16.java
@@ -1,18 +1,21 @@
 import java.io.*;
 import java.util.Scanner;
+import java.io.IOException;
 public class Task16 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Укажите путь к файлу .txt: ");
-        String i  = input.nextLine();
-        String s;
-        try (BufferedReader br = new BufferedReader(new FileReader(i))){
-        while((s = br.readLine()) != null) {
-            System.out.println(s);
-        }
+        String i = input.nextLine();
+        String s = null;
+        int linesCount = 0;
+        try (LineNumberReader br = new LineNumberReader(new FileReader(i))) {
+            while ((s = br.readLine()) != null) {
+                System.out.println(s);
+                 {linesCount++;}
+            }
         } catch (IOException exc) {
             System.out.println("Ошибка ввода-вывода: " + exc);
         }
-
+        System.out.println("Koличecтвo cтpok в фaйлe: " + linesCount);
     }
 }
--- Homework/src/Task18.java
+++ Homework/src/Task18.java
@@ -6,14 +6,18 @@ public class Task18 {
         System.out.println("Укажите путь к файлу .txt: ");
         String i  = input.nextLine();
         String s;
-        try (BufferedReader br = new BufferedReader(new FileReader(i))){
+        int linesCount = 0;
+
+        try (LineNumberReader br = new LineNumberReader(new FileReader(i))){
             while((s = br.readLine()) != null) {
                 System.out.println(s);
+                {linesCount++;}       //для подсчета строк
             }
         } catch (IOException exc) {
             System.out.println("Ошибка ввода-вывода: " + exc);
         }
 
+        System.out.println("\r\n" + "Koличecтвo cтpoк в фaйлe: " + linesCount);
         String str;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Признак конца ввода - строка 'stop'");
@@ -24,11 +28,8 @@ public class Task18 {
                 if(str.compareTo("stop") == 0) break;
                 str = str + "\r\n";
                 fw.write(str);
-            } while (str.compareTo("stop") != 0);
-        } catch(IOException exc) {
-
-        }
-
-
+            }
+            while (str.compareTo("stop") != 0);
+        } catch(IOException exc) {}
     }
 }
