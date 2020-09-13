75bd055613441b63a329cc460c8402fe914dbd51
Mirsaitov <Dinar.Mirsaitov@appline.ru>
2020-09-14 02:14:34 +0300

    Зад 18 исправлен

--- Homework/src/Task18.java
+++ Homework/src/Task18.java
@@ -1,36 +1,36 @@
 import java.io.*;
 import java.util.Scanner;
+
 public class Task18 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Укажите путь к файлу .txt: ");
-        String i  = input.nextLine();
+        String i = input.nextLine();
         String s;
         int linesCount = 0;
 
-        try (LineNumberReader br = new LineNumberReader(new FileReader(i))){
-            while((s = br.readLine()) != null) {
+        try (LineNumberReader br = new LineNumberReader(new FileReader(i))) {
+            while ((s = br.readLine()) != null) {
                 System.out.println(s);
-                {linesCount++;}       //для подсчета строк
+                {
+                    linesCount++;
+                }       //для подсчета строк
             }
         } catch (IOException exc) {
             System.out.println("Ошибка ввода-вывода: " + exc);
         }
 
         System.out.println("\r\n" + "Koличecтвo cтpoк в фaйлe: " + linesCount);
+
         String str;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Признак конца ввода - строка 'stop'");
-        try (FileWriter fw = new FileWriter(i)){
-            do {
-                System.out.print(": ");
+        try (FileWriter fw = new FileWriter(i)) {
+            for (int t = 0; t < linesCount; ++t) {
                 str = br.readLine();
-                if(str.compareTo("stop") == 0) break;
                 str = str + "\r\n";
                 fw.write(str);
             }
-            while (str.compareTo("stop") != 0);
-        } catch(IOException exc) {
+        } catch (IOException exc) {
             System.out.println("Ошибка ввода-вывода: " + exc);
         }
     }
